bfcadd35c4f63795593ac1e9acfdde1ffee9fddf
sandisya <143463315+sandisya@users.noreply.github.com>
2025-05-08 14:38:20 +0700

    Part 7

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BatchController;
 use App\Http\Controllers\StudentController;
 use App\Http\Controllers\TeacherController;
 use App\Http\Controllers\CourseController;
@@ -24,3 +25,4 @@ Route::resource('/students', StudentController::class);
 
 Route::resource('/teachers', TeacherController::class);
 Route::resource('/courses', CourseController::class);
+Route::resource('/batches', BatchController::class);
