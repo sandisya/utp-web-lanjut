bb3c1b17910418ca6ffe90ac178f542a51039301
sandisya <143463315+sandisya@users.noreply.github.com>
2025-05-08 17:58:26 +0700

    Part 12

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+
+use App\Models\Enrollment;
 use Illuminate\Http\Request;
 use App\Models\Payment;
 
@@ -21,8 +23,8 @@ class PaymentController extends Controller
      */
     public function create()
     {
-        $payments = Payment::pluck('enroll_id','id');
-        return view('payments.create', compact('payments'));
+        $enrollments = Enrollment::pluck('enroll_no','id');
+        return view('payments.create', compact('enrollments'));
     }
 
     /**
@@ -50,7 +52,8 @@ class PaymentController extends Controller
     public function edit(string $id)
     {
         $payments = Payment::find($id);
-        return view('payments.edit')->with('payments', $payments);
+        $enrollments = Enrollment::pluck('enroll_no','id');
+        return view('payments.edit', compact('payments', 'enrollments'));
     }
 
     /**
